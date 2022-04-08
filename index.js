be17d1d66e352c6a600e6984bff65c8ace5b874c
tshifhiwa <matombot@gmail.com>
2022-04-08 15:55:55 +0200

    make it better

--- index.js
+++ index.js
@@ -6,18 +6,19 @@ const app = express();
 
 // enable the static folder...
 app.use(express.static('public'));
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 // import the dataset to be used here
 
 const PORT = process.env.PORT || 4017;
 
 // API routes to be added here
-app.get('/api/garments', function(req, res){
+//app.get('/api/garments', function (req, res) {
     // note that this route just send JSON data to the browser
     // there is no template
-    res.json({garments});
-});
-app.get('/api/garments', function(req, res){
+  //  res.json({ garments });
+//});
+app.get('/api/garments', function (req, res) {
 
     const gender = req.query.gender;
     const season = req.query.season;
@@ -25,24 +26,24 @@ app.get('/api/garments', function(req, res){
     const filteredGarments = garments.filter(garment => {
         // if both gender & season was supplied
         if (gender != 'All' && season != 'All') {
-            return garment.gender === gender 
-                && garment.season === season;
-        } else if(gender != 'All') { // if gender was supplied
             return garment.gender === gender
-        } else if(season != 'All') { // if season was supplied
+                && garment.season === season;
+        } else if (gender != 'All') { // if gender was supplied
+            return garment.gender === gender
+        } else if (season != 'All') { // if season was supplied
             return garment.season === season
         }
         return true;
     });
 
-    res.json({ 
-        garments : filteredGarments
+    res.json({
+        garments: filteredGarments
     });
 });
 
-app.get('/api/garments/price/:price', function(req, res){
+app.get('/api/garments/price/:price', function (req, res) {
     const maxPrice = Number(req.params.price);
-    const filteredGarments = garments.filter( garment => {
+    const filteredGarments = garments.filter(garment => {
         // filter only if the maxPrice is bigger than maxPrice
         if (maxPrice > 0) {
             return garment.price <= maxPrice;
@@ -50,10 +51,39 @@ app.get('/api/garments/price/:price', function(req, res){
         return true;
     });
 
-    res.json({ 
-        garments : filteredGarments
+    res.json({
+        garments: filteredGarments
     });
 });
-app.listen(PORT, function() {
+app.post('/api/garments', (req, res) => {
+    // get the fields send in from req.body
+    const {
+        description,
+        img,
+        gender,
+        season,
+        price
+    } = req.body;
+    if(!description || !img || !price){
+        res.json({
+            status: 'error' ,
+            message: 'Required data not supplied' ,
+        });
+    }else {
+        garments.push({
+            description,
+            img,
+            gender,
+            season,
+            price
+        });
+        res.json({
+            status: 'success' ,
+            message:'New garment added' ,
+
+        });
+    }
+});
+app.listen(PORT, function () {
     console.log(`App started on port ${PORT}`)
 });
